eb18b56db50597f9dcc4f35c81f73622dfebb88f
Carlos Eduardo Vieira Santos <cadu.vs@hotmail.com>
2022-05-11 20:34:38 -0300

    added OccurrenceController

---
+++ Lighthouse/Controllers/OccurrenceController.cs
@@ -0,0 +1,29 @@
+﻿using Lighthouse.DAO;
+using Lighthouse.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Lighthouse.Controllers
+{
+    public class OccurrenceController : AbstractCrudController<OccurrenceViewModel>
+    {
+        public OccurrenceController()
+        {
+            GetNextId = false;
+            DAO = new OccurrenceDAO();
+        }
+        protected override void Validate(char op, OccurrenceViewModel sensor)
+        {
+            base.Validate(op, sensor);
+
+            if (sensor.Latitude < -90 || sensor.Latitude > 90)
+                ModelState.AddModelError("Latitude", "Latidude fora de alcance: o valor deve estar entre -90 e 90.");
+
+            if (sensor.Longitude < -180 || sensor.Longitude > 180)
+                ModelState.AddModelError("Longitude", "Longitude fora de alcance: o valor deve estar entre -180 e 180.");
+
+            if (sensor.DateReference > DateTime.Now)
+                ModelState.AddModelError("DateReference", "A data da ocorrência não pode ser futura.");
+        }
+    }
+}
