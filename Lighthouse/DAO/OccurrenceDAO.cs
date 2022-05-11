63607fd6745193aef6ad1125b173e168ac040768
Carlos Eduardo Vieira Santos <cadu.vs@hotmail.com>
2022-05-11 20:11:55 -0300

    added OcurrenceViewModel and OccurrenceDAO logic

---
+++ Lighthouse/DAO/OccurrenceDAO.cs
@@ -0,0 +1,37 @@
+﻿using Lighthouse.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Lighthouse.DAO
+{
+    public class OccurrenceDAO : AbstractCrudDAO<OccurrenceViewModel>
+    {
+        protected override void SetTable()
+        {
+            _tableName = "Ocurrence";
+        }
+
+        protected override OccurrenceViewModel RowToModel(DataRow row)
+        {
+            return new OccurrenceViewModel()
+            {
+                Id = Convert.ToInt32(row["Id"]),
+                Longitude = Convert.ToDouble(row["Longitude"]),
+                Latitude = Convert.ToDouble(row["Latitude"]),
+                DateReference = Convert.ToDateTime(row["DateReference"])
+            };
+        }
+
+        protected override SqlParameter[] GetParameters(OccurrenceViewModel model)
+        {
+            return new SqlParameter[]
+            {
+                new SqlParameter("Id", model.Id),
+                new SqlParameter("Longitude", model.Longitude),
+                new SqlParameter("Latitude", model.Latitude),
+                new SqlParameter("DateReference", model.DateReference)
+            };
+        }
+    }
+}
---
+++ Lighthouse/Models/OccurrenceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lighthouse.Models
+{
+    public class OccurrenceViewModel : AbstractCrudViewModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime DateReference { get; set; }
+    }
+}
