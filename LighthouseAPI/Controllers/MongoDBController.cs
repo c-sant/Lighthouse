d089af3d8cacb306a2d1d7516ac55e9615993612
Guilherme Turtera <guiturtera@hotmail.com>
2022-05-18 14:02:11 -0300

    improvement: started mongodb parser

---
+++ LighthouseAPI/Controllers/MongoDBController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LighthouseAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MongoDBController : ControllerBase
+    {
+        private readonly ILogger<MongoDBController> _logger;
+
+        public MongoDBController(ILogger<MongoDBController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("MongoToSqlServer")]
+        public IActionResult Get()
+        {
+            return new JsonResult(new { Works = true });
+        }
+    }
+}
