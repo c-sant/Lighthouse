667e12e7344208318271417ee939ff59f863ca21
NathanV-0109 <nathanvs@outlook.com>
2022-05-20 22:50:32 -0300

    add preliminier dashboards
     - I'm Lost

---
+++ Lighthouse/Controllers/DashBoardController.cs
@@ -0,0 +1,23 @@
+﻿using Lighthouse.DAO;
+using Lighthouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lighthouse.Controllers
+{
+    public class DashBoardController : AbstractCrudController<SensorViewModel>
+    {
+        public DashBoardController()
+        {
+            GetNextId = false;
+            DAO = new SensorDAO();
+            RequiresLogin = true;
+        }
+
+        public IActionResult GetSensors()
+        {
+            var sensors = DAO.ReadAll();
+            return Json(new { dados = sensors });
+        }
+ 
+    }
+}
